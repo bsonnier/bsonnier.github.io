01ad89a7898579be8da73843af492e6ef91ac6bc
bsonnier <bsonnier@spscc.edu>
2018-06-14 09:24:38 +0800

    Refactor Reorder Script
    Refactor reordercode.py

--- docs/reordercode.py
+++ docs/reordercode.py
@@ -1,9 +1,19 @@
-codefile = open("./bsonnier.github.io/docs/codetag", "r")
-lines = codefile.readlines()
-print(lines)
-codefile.close()
+def reorderfile(filename):
+    codefile = open(filename, "r", encoding='utf-8')
+    lines = codefile.readlines()
+    lines[-1] = lines[-1] + '\n'
+    print(lines)
+    codefile.close()
 
-codefilewr = open("./bsonnier.github.io/docs/codetag", "w")
-codefilewr.writelines(sorted(lines))
-print(sorted(lines))
-codefilewr.close()
+    codefilewr = open(filename, "w", encoding='utf-8')
+    sortedlines = sorted(lines)
+    sortedlines[-1] = sortedlines[-1][0:-1]
+    codefilewr.writelines(sortedlines)
+    print(sortedlines)
+    codefilewr.close()
+
+
+if __name__ == '__main__':
+    reorderfile("./bsonnier.github.io/docs/codefilter")
+    reorderfile("./bsonnier.github.io/docs/codetag")
+    reorderfile("./bsonnier.github.io/docs/namelist")
