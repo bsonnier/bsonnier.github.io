6c70c76b0cccfdb8117eed9f673d217199b628a8
bsonnier <bsonnier@spscc.edu>
2018-06-18 15:35:14 +0800

    Add Name Extract
    Add name list extract function.

---
+++ docs/extractnamelist.py
@@ -0,0 +1,21 @@
+
+from bs4 import BeautifulSoup
+
+
+def extractname(filename, filenamewr):
+    namelist = []
+    soup = BeautifulSoup(open(filename, encoding='utf-8'), "lxml")
+    for star in soup.find_all('span', 'star'):
+        namelist.append(star.string + '\n')
+
+    codefilewr = open(filenamewr, "w", encoding='utf-8')
+    sortedlines = sorted(namelist)
+    sortedlines[-1] = sortedlines[-1][0:-1]
+    codefilewr.writelines(sortedlines)
+    print(sortedlines)
+    codefilewr.close()
+
+
+if __name__ == '__main__':
+    extractname("./bsonnier.github.io/docs/favstars-mjyang-1529301206.html",
+                "./bsonnier.github.io/docs/favstars-mjyang")
